208d0a27842b1288b2721da15f8d90b842f8e4db
dev-kmg2331 <kmg2331@gmail.com>
2023-02-20 23:47:11 +0900

    모달창 애니메이션 구현 02/20 11:47

--- workspace/kizSookSook/WebContent/js/mypage/mypage.js
+++ workspace/kizSookSook/WebContent/js/mypage/mypage.js
@@ -1,21 +1,72 @@
 let stageArr = new Array(
-    'stage-list/trip-history.html',
-    'stage-list/cash-history.html',
-    'stage-list/my-info.html',
-    'stage-list/my-review.html',
-    'stage-list/my-qna.html',
-    'stage-list/unregister.html'
+    "stage-list/trip-history.html",
+    "stage-list/cash-history.html",
+    "stage-list/my-info.html",
+    "stage-list/my-review.html",
+    "stage-list/my-qna.html",
+    "stage-list/unregister.html"
 );
 
-const $stage = $('.stage');
+const $stage = $(".stage");
+let isModalActive = false;
+
+let modalDisableService = (function () {
+    function disable($dom) {
+        console.log($dom);
+
+        $dom.animate(
+            {
+                top: "45px",
+                opacity: 0,
+            },
+            500
+        );
+
+        isModalActive = false;
+    }
+
+    return { disable: disable };
+})();
+
+let modalActivateService = (function () {
+    function activate($dom) {
+        console.log($dom);
+
+        $dom.animate(
+            {
+                top: "35px",
+                opacity: 1,
+            },
+            500
+        );
+
+        isModalActive = true;
+    }
+
+    return { activate: activate };
+})();
 
 let categoryModalService = (function () {
     function load() {
-        $('.theme-name').each(function (i, e) {
-            $(e).on('click', function () {
-                console.log($(this).text());
+        var $theme = $(".theme-name");
+        var $themeName = $("#theme-selecter");
+        var $modal = $theme.closest("div.selecter-options");
 
-                $('#themeName').text($(this).text());
+        $themeName.on("click", () => {
+            if (isModalActive) {
+                modalDisableService.disable($modal);
+            } else modalActivateService.activate($modal);
+        });
+
+        $theme.each(function (i, e) {
+            $(e).on("click", function () {
+                var $value = $(this).text();
+
+                $("#themeName").text($value);
+
+                modalDisableService.disable($modal);
+
+                $("input[name='theme']").val($value);
             });
         });
     }
@@ -29,10 +80,10 @@ $.get(stageArr[0], function (data) {
     categoryModalService.load();
 });
 
-$('.item-name').each((i, e) => {
+$(".item-name").each((i, e) => {
     var index = i;
 
-    $(e).on('click', function (e) {
+    $(e).on("click", function (e) {
         e.preventDefault();
 
         $stage.load(stageArr[index], () => {
