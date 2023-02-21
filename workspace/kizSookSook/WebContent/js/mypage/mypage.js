00603df1d0dc17d907771e30ce9a93eb7c15bf7a
kmg2331 <kmg2331@gmail.com>
2023-02-22 03:04:49 +0900

    02/22 03:04 강민구 작업완료 내정보 수정 정규식 적용완료

--- workspace/kizSookSook/WebContent/js/mypage/mypage.js
+++ workspace/kizSookSook/WebContent/js/mypage/mypage.js
@@ -4,10 +4,10 @@ $.get(arraysObj.stageArr[0], function (data) {
     functionsObj.modal.service.excute();
 });
 
-$(".item-name").each((i, e) => {
+$('.item-name').each((i, e) => {
     var index = i;
 
-    $(e).on("click", function (e) {
+    $(e).on('click', function (e) {
         e.preventDefault();
 
         jqueryDomObj.load().$stage.load(arraysObj.stageArr[index], () => {
@@ -15,32 +15,32 @@ $(".item-name").each((i, e) => {
 
             switch (index) {
                 case 0:
-                    console.log("케이스 0들어옴");
+                    console.log('케이스 0들어옴');
                     functionsObj.modal.service.excute();
                     break;
                 case 1:
-                    console.log("케이스 1들어옴");
+                    console.log('케이스 1들어옴');
                     functionsObj.modal.service.excute();
                     break;
                 case 2:
-                    console.log("케이스 2들어옴");
-                    checkInputs();
+                    console.log('케이스 2들어옴');
+                    functionsObj.myInfo.inputCheckService.excute();
                     break;
                 case 3:
-                    console.log("케이스 3들어옴");
+                    console.log('케이스 3들어옴');
                     functionsObj.modal.service.excute();
                     break;
                 case 4:
-                    console.log("케이스 4들어옴");
+                    console.log('케이스 4들어옴');
                     functionsObj.modal.service.excute();
                     break;
                 case 5:
-                    console.log("케이스 5들어옴");
+                    console.log('케이스 5들어옴');
                     functionsObj.unregister.checkService.excute();
                     functionsObj.unregister.warningsService.excute();
                     break;
                 default:
-                    alert("페이지 전환 오류!");
+                    alert('페이지 전환 오류!');
                     break;
             }
         });
