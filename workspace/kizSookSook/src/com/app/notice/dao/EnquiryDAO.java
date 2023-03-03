70738467201cf8ae80b5f4a1a9f3bca419c562ee
coding-moon <dawn43214@gmail.com>
2023-03-03 15:14:06 +0900

    새 커밋

---
+++ workspace/kizSookSook/src/com/app/notice/dao/EnquiryDAO.java
@@ -0,0 +1,21 @@
+package com.app.notice.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.app.notice.domain.CustomerEnquiryVO;
+
+public class EnquiryDAO {
+	public SqlSession sqlSession;
+	
+	// 문의 등록
+		public void insert(CustomerEnquiryVO customerEnquiryVO) {
+			sqlSession.insert("notice.insert", customerEnquiryVO);
+		}
+		
+		// 문의 등록번호 조회
+		public Long getCurrentSequence() {
+			return sqlSession.selectOne("notice.getCurrentSequence");
+			
+		}
+
+}
